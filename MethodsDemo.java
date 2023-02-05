ddf026f51f59c4f2618fed2103cacb0b7fe13104
jtsyganov <jtsyganov@gmail.com>
2023-02-05 10:04:49 -0500

    class15

---
+++ MethodsDemo.java
@@ -0,0 +1,12 @@
+package class15;
+
+import java.util.Scanner;
+
+public class MethodsDemo {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+    }
+}
---
+++ Repl76.java
@@ -0,0 +1,17 @@
+package class15;
+
+import java.util.Scanner;
+
+public class Repl76 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int[] number;
+
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
